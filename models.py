4b5d500c00848f338a56f6cfc0ecd389d325f7d4
lou <louamaya@me.com>
2020-12-13 15:39:22 -0800

    added post model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize variable to run SQLAlchemy
@@ -11,7 +12,7 @@ def connect_db(app):
     db.init_app(app)
 
 
-class Blogly(db.Model):
+class User(db.Model):
     """Creates the blogly table"""
 
     __tablename__ = "users"
@@ -31,3 +32,35 @@ class Blogly(db.Model):
 
     image_url = db.Column(db.Text(),
                           nullable=False)
+
+    post = db.relationship("Post", backref="user", cascade="all,delete-orphan")
+
+
+class Post(db.Model):
+    """Creates the model table"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    title = db.Column(db.String(50),
+                      nullable=False)
+
+    content = db.Column(db.Text,
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
+
+    @property
+    def this_date(self):
+        """Generates a formatted date"""
+
+        return self.created_at.strftime("%a %b %-d %Y, %-I:%M %p")
